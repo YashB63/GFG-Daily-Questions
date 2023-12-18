15faf9362b4b34acf3f76ea819467199ca21d30f
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-12-18 18:14:09 +0530

    Add files via upload

---
+++ Day 12/Check if array is sorted/sorted_array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        
+        for i in range(1, n):
+            if arr[i] < arr[i - 1]:
+                return False
+        return True
