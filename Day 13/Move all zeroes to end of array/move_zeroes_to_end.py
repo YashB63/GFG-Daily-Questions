4f21af60524f70ea067ef15007d89e36487e6f27
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-12-19 20:10:53 +0530

    Add files via upload

---
+++ Day 13/Move all zeroes to end of array/move_zeroes_to_end.py
@@ -0,0 +1,9 @@
+class Solution:
+	def pushZerosToEnd(self,arr, n):
+    	
+    	non_zero_index = 0
+    	
+        for i in range(n):
+            if arr[i] != 0:
+                arr[i], arr[non_zero_index] = arr[non_zero_index], arr[i]
+                non_zero_index += 1
