48a1ee2e8415bff63702a9149d9c50b277eb1b4b
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-02-18 20:22:31 +0530

    Add files via upload

---
+++ Day 73/Find all Critical Connections in the Graph/find_all_critical_connections_in_the_graphs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def criticalConnections(self, v, adj):
+        
+        def dfs(u, parent, disc, low, bridges):
+            nonlocal time
+            disc[u] = low[u] = time
+            time += 1
+            for v in adj[u]:
+                if disc[v] == -1:
+                    dfs(v, u, disc, low, bridges)
+                    low[u] = min(low[u], low[v])
+                    if low[v] > disc[u]:
+                        bridges.append((min(u, v), max(u, v)))
+                elif v != parent:
+                    low[u] = min(low[u], disc[v])
+                    
+        disc = [-1] * v
+        low = [-1] * v
+        time = 0
+        bridges = []
+        for i in range(v):
+            if disc[i] == -1:
+                dfs(i, -1, disc, low, bridges)
+        return sorted(bridges)
