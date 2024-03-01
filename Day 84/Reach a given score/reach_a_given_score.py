833dfbe47ff70db3024ff178caa16c7caf729863
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-03-01 23:29:21 +0530

    Add files via upload

---
+++ Day 84/Reach a given score/reach_a_given_score.py
@@ -0,0 +1,12 @@
+class Solution:
+    def count(self, n: int) -> int:
+        
+        arr=[0]*(n+1)
+        arr[0]=1
+        for i in range(3,n+1):
+            arr[i]+=arr[i-3]
+        for i in range(5,n+1):
+            arr[i]+=arr[i-5]
+        for i in range(10,n+1):
+            arr[i]+=arr[i-10]
+        return arr[-1]
