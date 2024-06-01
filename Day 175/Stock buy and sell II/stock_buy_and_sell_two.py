20ec17de53fb070d9d61c71e71d83d1f4288e45b
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-06-01 19:19:33 +0530

    Add files via upload

---
+++ Day 175/Stock buy and sell II/stock_buy_and_sell_two.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def stockBuyAndSell(self, n : int, prices : List[int]) -> int:
+       
+        max_profit = 0
+    
+        for i in range(1, len(prices)):
+            
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return max_profit
