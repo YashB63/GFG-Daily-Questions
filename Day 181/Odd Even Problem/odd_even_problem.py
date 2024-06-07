d12713bef8561f2c224bd9a1778b98e2436f9e8e
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-06-07 08:05:24 +0530

    Add files via upload

---
+++ Day 181/Odd Even Problem/odd_even_problem.py
@@ -0,0 +1,16 @@
+class Solution:
+    def oddEven(self, s : str) -> str:
+        
+        even="bdfhjlnprtvxz"
+        ans=0
+        ss=set(s)
+        for i in ss:
+            if i in even:
+                if s.count(i)%2==0:
+                    ans+=1
+            else:
+                if s.count(i)%2!=0:
+                    ans+=1
+        if ans%2==0:
+            return "EVEN"
+        return "ODD"
