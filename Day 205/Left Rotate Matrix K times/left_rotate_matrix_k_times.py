ce90a530556f853b9bf1273077457aa9928a7fa6
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-07-01 22:27:25 +0530

    Add files via upload

---
+++ Day 205/Left Rotate Matrix K times/left_rotate_matrix_k_times.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateMatrix(self, k, mat):
+        
+        n = len(mat)
+        k = k % len(mat[0])
+        for i in range(n):
+            mat[i] = mat[i][k:] + mat[i][:k]
+        return mat
