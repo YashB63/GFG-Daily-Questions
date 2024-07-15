ffe607df94e5c3c709b6b2931f0bb488f58e1098
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-07-15 18:56:30 +0530

    Add files via upload

---
+++ Day 219/Count Palindromic Subsequences/count_palindromic_subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    
+    def countPS(self,string):
+        
+        n = len(string)
+        md = int(1e9+7)
+        dp = [1]*n
+        for i in range(1,n):
+            cs = 0
+            for j in range(i-1,-1,-1):
+                tmp = dp[j]
+                if(string[i] == string[j]):
+                    dp[j] = dp[j] + cs + 1
+                cs += (tmp % md)
+        
+        return (sum(dp) % md)
