96f200d1949b76e1a54b9c12c61298b567c49798
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-08-12 22:20:44 +0530

    Add files via upload

---
+++ Day 247/Maximize Array Value After Rearrangement/maximize_array_value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def Maximize(self, arr): 
+        
+        MOD = 10**9 + 7
+        
+        arr.sort()
+        
+        result = 0
+        
+        for i in range(len(arr)):
+            result = (result + arr[i] * i) % MOD
+        
+        return result
