d4ead26a5e22d17baa3712311a743f532801a0ac
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-09-16 18:26:01 +0530

    Add files via upload

---
+++ Day 282/Circle of strings/circle_of_strings.py
@@ -0,0 +1,53 @@
+class Solution:
+    def dfs(self, node, adj, visited):
+        visited[node] = True
+        for u in adj[node]:
+            if not visited[u]:
+                self.dfs(u, adj, visited)
+
+    def isConnected(self, adj, indegree):
+        visited = [False] * 26
+        start = -1
+        
+        for i in range(26):
+            if indegree[i] > 0:
+                start = i
+                break
+        
+        if start == -1:
+            return True
+        
+        self.dfs(start, adj, visited)
+        
+        for i in range(26):
+            if indegree[i] > 0 and not visited[i]:
+                return False
+        
+        return True
+
+    def isCircle(self, arr):
+        N = len(arr)
+        adj = [[] for _ in range(26)]
+        adjr = [[] for _ in range(26)]
+        indegree = [0] * 26
+        outdegree = [0] * 26
+        
+        for word in arr:
+            u = ord(word[0]) - ord('a')
+            v = ord(word[-1]) - ord('a')
+            adj[u].append(v)
+            adjr[v].append(u)
+            outdegree[u] += 1
+            indegree[v] += 1
+
+        for i in range(26):
+            if indegree[i] != outdegree[i]:
+                return 0
+
+        if not self.isConnected(adj, indegree):
+            return 0
+
+        if not self.isConnected(adjr, outdegree):
+            return 0
+        
+        return 1
