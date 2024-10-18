5ce9e7e915f0265c7803d8ae46bbff7f211f8c22
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-10-18 22:24:16 +0530

    Add files via upload

---
+++ Day 314/Two Smallests in Every Subarray/two_smallests_in_every_subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pairWithMaxSum(self, arr):
+        if len(arr) < 2:
+            return -1
+        
+        max_sum = float('-inf')
+        
+        for i in range(len(arr) - 1):
+            current_sum = arr[i] + arr[i + 1]
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum
