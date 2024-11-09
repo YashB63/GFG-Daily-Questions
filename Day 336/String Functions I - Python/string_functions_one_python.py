01a3f42515a34416f7ae6277ad40518e4727254b
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-11-09 21:14:05 +0530

    Add files via upload

---
+++ Day 336/String Functions I - Python/string_functions_one_python.py
@@ -0,0 +1,11 @@
+def trim(str):
+    return str.strip()      
+
+def exists(str, x):
+    return str.find(x)    
+
+def titleIt(str):
+    return str.title()     
+
+def casesSwap(str):
+    return str.swapcase()    
