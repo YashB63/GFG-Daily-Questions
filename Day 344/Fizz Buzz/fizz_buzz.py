546653e2b6cf6d6a68ffe474d6e1aabf60f04f6f
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2024-11-17 23:13:53 +0530

    Add files via upload

---
+++ Day 344/Fizz Buzz/fizz_buzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n : int):
+        l = []
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                l.append("FizzBuzz")
+            elif i%3==0:
+                l.append("Fizz")
+            elif i%5==0:
+                l.append("Buzz")
+            else:
+                l.append(i)
+        return l
