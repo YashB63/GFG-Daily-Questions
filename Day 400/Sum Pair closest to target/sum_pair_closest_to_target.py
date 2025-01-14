c43c4a030042e690d32e00c774c9e5e30f6b0728
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-01-14 23:26:26 +0530

    Add files via upload

---
+++ Day 400/Sum Pair closest to target/sum_pair_closest_to_target.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumClosest(self, arr, target):
+        arr.sort()
+        l, r, minDiff, res = 0, len(arr) - 1, float('inf'), []
+        while l < r:
+            s = arr[l] + arr[r]
+            if abs(target - s) < minDiff:
+                minDiff, res = abs(target - s), [arr[l], arr[r]]
+            l += s < target
+            r -= s >= target
+        return res
