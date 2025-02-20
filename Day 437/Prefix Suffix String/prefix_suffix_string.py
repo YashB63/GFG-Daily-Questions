003e77ed955b5a515539aa266dbbd68ef5595d26
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-02-20 22:52:21 +0530

    Add files via upload

---
+++ Day 437/Prefix Suffix String/prefix_suffix_string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def prefixSuffixString(self, s1, s2) -> int:
+        ans = 0
+        i=0
+        j=0
+        m=len(s1)
+        n=len(s2)
+        while(i<m and j<n):
+            if s2[j] in s1[i]:
+                l = len(s2[j])
+                if s2[j]==s1[i][:l] or s2[j]==s1[i][-l:]:
+                    ans+=1
+                    i=0
+                    j+=1
+                else: i+=1
+            else: i+=1
+        return ans
