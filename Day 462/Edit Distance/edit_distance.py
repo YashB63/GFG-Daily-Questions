de53587f78e5c9a5d76edd806bc120397dedc14d
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-03-17 22:03:11 +0530

    Add files via upload

---
+++ Day 462/Edit Distance/edit_distance.py
@@ -0,0 +1,20 @@
+class Solution:
+	def editDistance(self, s1, s2):
+        n1=len(s1)
+        n2=len(s2)
+        dp = [[float("inf")]*(len(s2)+1) for _ in range(len(s1)+1)]
+        
+        for j in range(n2+1):
+            dp[n1][j] = n2-j
+        
+        for i in range(n1+1):
+            dp[i][n2] = n1-i
+            
+        for i in range(n1-1,-1,-1):
+            for j in range(n2-1,-1,-1):
+                if s1[i]==s2[j]:
+                    dp[i][j]=dp[i+1][j+1]
+                else:
+                    dp[i][j]=1+min(dp[i+1][j+1],dp[i+1][j],dp[i][j+1])
+         
+        return dp[0][0]
