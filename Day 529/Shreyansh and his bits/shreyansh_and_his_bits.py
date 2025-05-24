de3683df0edec9491522879db0502de848438579
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-05-24 21:02:11 +0530

    Add files via upload

---
+++ Day 529/Shreyansh and his bits/shreyansh_and_his_bits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def ncr(self,n,r):
+        if r>n:
+            return 0
+        if r==0 or r==n:
+            return 1
+        if self.cache[n][r]!=-1:
+            return self.cache[n][r]
+        
+        res=self.ncr(n-1,r-1)+self.ncr(n-1,r)
+        self.cache[n][r]=res
+        return res
+    
+    def count (self, N):
+        self.cache=[[-1]*(40) for _ in range(40)]
+        res=0
+        ones=0
+        bits=0
+        
+        while N!=0:
+            if N&1:
+                res+=self.ncr(bits,ones+1)
+                ones+=1
+            bits+=1
+            N>>=1
+        return res
