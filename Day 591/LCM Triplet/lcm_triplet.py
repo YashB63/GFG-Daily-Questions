37040c9a6be41fd0446f98fd0c14554676973e80
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-07-25 20:53:45 +0530

    Add files via upload

---
+++ Day 591/LCM Triplet/lcm_triplet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lcmTriplets(self, n):
+        if n < 3:
+            return n
+
+        if n % 2 != 0:
+            return n * (n - 1) * (n - 2)
+
+        if n % 3 != 0:
+            return n * (n - 1) * (n - 3)
+
+        return (n - 1) * (n - 2) * (n - 3)
