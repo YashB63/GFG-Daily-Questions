64f96aac066f101f2fcc7c650150e545475a89c2
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-07-30 21:03:50 +0530

    Add files via upload

---
+++ Day 596/Restricted Pacman/restricted_pacman.py
@@ -0,0 +1,30 @@
+class Solution:
+    def candies(self, m, n):
+        i = 0
+        X = (m * n) - m - n
+
+        queue = []
+        
+        queue.append(X)
+        
+        set = {X}
+        
+        count = 0
+        while (len(queue) > 0):
+
+            curr = queue[0]
+            queue.remove(queue[0])
+
+            count += 1
+            
+            key = curr - m
+            if (key > 0 and key not in set):
+                queue.append(key)
+                set.add(key)
+        
+            key = curr - n
+            if (key > 0 and key not in set):
+                queue.append(key)
+                set.add(key)
+
+        return count
