64fea0d7b5c5cf15518d49694929b0ff5a2cee06
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-08-13 22:16:38 +0530

    Add files via upload

---
+++ Day 609/Palindrome Sentence/palindrome_sentence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalinSent(self, s):
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+
+            elif not s[j].isalnum():
+                j -= 1
+
+            elif s[i].lower() == s[j].lower():
+                i += 1
+                j -= 1
+
+            else:
+                return False
+
+        return True
