4c4eeae2ce42cc20d1b46bc8e216832135fd17ea
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-08-14 22:07:32 +0530

    Add files via upload

---
+++ Day 610/Roman Number to Integer/roman_number_to_integer.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def value(self, r):
+        if (r == 'I'):
+            return 1
+        if (r == 'V'):
+            return 5
+        if (r == 'X'):
+            return 10
+        if (r == 'L'):
+            return 50
+        if (r == 'C'):
+            return 100
+        if (r == 'D'):
+            return 500
+        if (r == 'M'):
+            return 1000
+        return -1
+
+    def romanToDecimal(self, s):
+        res = 0
+        i = 0
+        while (i < len(s)):
+            s1 = self.value(s[i])
+
+            if (i + 1 < len(s)):
+
+                s2 = self.value(s[i + 1])
+
+                if (s1 >= s2):
+                    res = res + s1
+                    i = i + 1
+                else:
+                    res = res + s2 - s1
+                    i = i + 2
+            else:
+                res = res + s1
+                i = i + 1
+
+        return res
