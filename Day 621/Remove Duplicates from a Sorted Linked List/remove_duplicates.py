30143e12aa51719a8879a7a020db1a031cd1483e
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-08-25 23:52:56 +0530

    Add files via upload

---
+++ Day 621/Remove Duplicates from a Sorted Linked List/remove_duplicates.py
@@ -0,0 +1,8 @@
+def removeDuplicates(head):
+    curr_node = head
+
+    while curr_node.next:
+        if (curr_node.data == curr_node.next.data):
+            curr_node.next = curr_node.next.next
+        else:
+            curr_node = curr_node.next
