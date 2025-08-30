4afa3c2325642719749aa13c7530b05bce58c3c5
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-08-30 23:58:38 +0530

    Add files via upload

---
+++ Day 625/Maximize the minimum difference between k elements/maximize_the_minimum_difference.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPossible(self, arr, k, mid):
+        count = 1
+        last = arr[0]
+
+        for i in range(1, len(arr)):
+            if arr[i] - last >= mid:
+                count += 1
+                last = arr[i]
+            if count == k:
+                return True
+
+        return False
+
+    def maxMinDiff(self, arr, k):
+        arr.sort()
+
+        low = 0
+        high = arr[-1] - arr[0]
+        answer = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if self.isPossible(arr, k, mid):
+                answer = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return answer
