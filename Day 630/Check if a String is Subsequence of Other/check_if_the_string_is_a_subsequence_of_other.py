250b24a81ffdd6cbfc0556fc9aec9ebe777d0b44
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-09-14 23:56:47 +0530

    Add files via upload

---
+++ Day 630/Check if a String is Subsequence of Other/check_if_the_string_is_a_subsequence_of_other.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubSeq(self, s1, s2):
+        n, m = len(s1), len(s2)
+        i, j = 0, 0
+        while (i < n and j < m):
+            if (s1[i] == s2[j]):
+                i += 1
+            j += 1
+
+        return i == n
