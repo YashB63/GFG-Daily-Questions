a406b86e51bb4737d6d217243097a189fbb6de06
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2025-09-19 21:00:36 +0530

    Add files via upload

---
+++ Day 634/Container With Most Water/container_with_most_water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxWater(self, arr):
+        left = 0
+        right = len(arr) - 1
+        res = 0
+        
+        while left < right:
+            water = min(arr[left], arr[right]) * (right - left)
+            res = max(res, water)
+
+            if arr[left] < arr[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return res
