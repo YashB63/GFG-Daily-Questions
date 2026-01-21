1ea3decaa2e643a0111d70dc9e299d25a09f5a00
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2026-01-21 21:20:50 +0530

    Add files via upload

---
+++ Day 712/Arithmetic Operators/arithmetic_operators.py
@@ -0,0 +1,14 @@
+x=int(input())
+y=int(input())
+
+# Perform addition x+y below
+p = x + y
+# Perform subtraction x-y below
+q = x - y
+# Perform multiplication x*y below
+r = x * y
+# Perform integer divison x//y below
+s = x // y
+# Perform modulo operation x%y below
+t = x % y
+print(p, q, r, s, t)
