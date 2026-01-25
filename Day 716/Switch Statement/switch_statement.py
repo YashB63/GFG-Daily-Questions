effb32a027cc8799120c5cf6a4c4f4502d58b5b9
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2026-01-25 22:05:19 +0530

    Add files via upload

---
+++ Day 716/Switch Statement/switch_statement.py
@@ -0,0 +1,14 @@
+def utility(number):
+    num_dict = {
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    }
+    
+    return num_dict.get(number, "Unknown")
