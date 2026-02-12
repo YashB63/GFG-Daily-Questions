851e74afea2c715a8ad816c3e811a3c1dbc425bb
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2026-02-12 21:22:46 +0530

    Add files via upload

---
+++ Day 732/K Sized Subarray Maximum/k_sized_subarray_maximum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        st=[]
+        l=len(arr)
+        ans=[0 for _ in range(l-k+1)]
+        def pus(st,ele):
+            while st and st[-1]<ele:
+                st.pop()
+            st.append(ele)
+            
+        def po(st,ele):
+            if st and st[0]==ele:
+                st.pop(0)
+                
+        for i in range(k-1):
+            pus(st,arr[i])
+            
+        for i in range(k-1,l):
+            pus(st,arr[i])
+            ans[i-k+1]=st[0]
+            po(st,arr[i-k+1])
+            
+        return ans
