c163694e1e5dfd27689898b2a048adcb8ec71cc1
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2026-02-23 21:57:48 +0530

    Add files via upload

---
+++ Day 742/Calculator/calculator.py
@@ -0,0 +1,9 @@
+def utility(a, b, opr):
+    if opr == 1:
+        print(str(a+b),end="")
+    elif opr ==2:
+        print(str(a-b),end="")
+    elif opr ==3:
+        print (str(a*b),end="")
+    else:
+     print("Invalid Input",end="") 
