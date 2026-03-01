57f9701e7a1d5fb6e227a794397bd51982900fe1
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2026-03-01 21:43:03 +0530

    Add files via upload

---
+++ Day 748/Form the Largest Number/form_the_largest_number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLargest(self, arr):
+	    import functools
+	    arr = [str(e) for e in arr]
+	    def cmp(e1, e2):
+	        a = e1+e2
+	        b = e2+e1
+	        if a > b:
+	            return -1
+	        elif a == b:
+	            return 0
+	        else:
+	            return 1
+	            
+        arr.sort(key=functools.cmp_to_key(cmp))
+        s = "".join(arr).lstrip("0")
+        return s if s else "0"
